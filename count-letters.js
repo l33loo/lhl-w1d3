88bd394530bd9c3c2e1daa3f53e11f2ed1b7a489
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-03-14 20:14:56 +0000

    Synthesized comments

--- count-letters.js
+++ count-letters.js
@@ -2,20 +2,20 @@
 /* A function that returns an object containing all the unique
 characters that exist in a given string, and the frequency of
 these characters in said string.*/
-function countLetters(string) {
+function countLetters(str) {
 
   /* Create an array that contains all the characters in string,
   in lowercase and excluding whitespace.*/
-  var letterArr = string.replace(/\s/g, "").toLowerCase().split("");
+  var letterArr = str.replace(/\s/g, "").toLowerCase().split("");
 
   var objLetterFrequ = {};
 
   // Loop through the letter array and add each element as a key in the object.
   for (var i = 0; i < letterArr.length; i++) {
 
-    /* If the key already exists, update the value that represents the number
-    of times the character appears in the letter array (or string).*/
+    /* If the key already exists, increment the letter frequency counter.*/
     if (objLetterFrequ[letterArr[i]]) {
+
       objLetterFrequ[letterArr[i]]++;
 
     /* If the key doesn't already exist, create it and set its value to 1. */

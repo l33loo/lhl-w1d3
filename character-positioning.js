133c8700748fa3089f48a65e4089e78a411e83c4
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-03-15 00:22:22 +0000

    Streamline the code

--- character-positioning.js
+++ character-positioning.js
@@ -6,40 +6,30 @@ just one number to represent its number of occurrences, but rather (potentially)
 multiple numbers to represent all the places in the string that it shows up.
 */
 
-function countLetters(str) {
+function getLetterIndeces(str) {
 
   // Convert the string to lowercase.
   str = str.toLowerCase();
 
-  // Create an object that will store the string characters and their indeces.
+  /* Create an object that will store the string characters as properties,
+  and the characters' indeces as values. */
   var charIdxObj = {};
 
   // Loop through the string characters.
   for (var i = 0; i < str.length; i++) {
 
-    // Create an array that stores the indeces of a given letter.
-    var charIdxArr = [];
+    /* If a property already exists for that character,
+    push the character's index into the property */
+    if (charIdxObj[str[i]]) {
+      charIdxObj[str[i]].push(i);
 
-    /* If the object property doesn't exist yet, compare the character to
-    itself and to each subsequent character. */
-    if (!charIdxObj[str[i]]) {
-      for (var j = i; j < str.length; j++) {
-
-        /* If both characters match, add to the index array the index of the character
-        to which the initial character is compared. */
-        if (str[i] === str[j]) {
-          charIdxArr.push(str.indexOf(str[j], j));
-        }
-      }
-
-      // Create the property for the letter and assign it's value to be the index array.
-      charIdxObj[str[i]] = charIdxArr;
+    /* If the property doesn't already exist, create it
+    and use the index as its value. */
+    } else {
+      charIdxObj[str[i]] = [i];
     }
   }
-
-  // Delete the object property that represents the space character.
   delete charIdxObj[" "];
-
   return charIdxObj;
 }
 
